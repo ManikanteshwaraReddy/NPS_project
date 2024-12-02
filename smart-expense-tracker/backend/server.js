d13fe70c06be858351b819f9e301b0286cb89ab1
ManikanteshwaraReddy <22bd1a0529@gmail.com>
2024-12-03 01:01:46 +0530

    files added

---
+++ smart-expense-tracker/backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
